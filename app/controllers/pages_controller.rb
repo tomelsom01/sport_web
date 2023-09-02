657f409d751cf082a3dbdd07954034724c46b5f1
tomelsom <te28@hotmail.com>
2023-09-02 23:33:57 +0100

    added crud in the controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,32 @@
 class PagesController < ApplicationController
   def index
+    @post = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    @post.save
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
   end
 end
---
+++ lib/main.rb
@@ -0,0 +1,5 @@
+#  1342
+# initiate a counter
+# create a loop until the number reaches 0
+# if even / 2 || odd - 1
+# count and display the steps
