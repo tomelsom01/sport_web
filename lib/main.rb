2e67f5c44c7182d2b9da835d5cf05b3b7b0dfce5
tomelsom <te28@hotmail.com>
2023-09-08 16:50:16 +0100

    adding crud to controller

--- lib/main.rb
+++ lib/main.rb
@@ -3,3 +3,19 @@
 # create a loop until the number reaches 0
 # if even / 2 || odd - 1
 # count and display the steps
+
+def number_of_steps(num)
+  n = 0
+  while num > 0
+    if num.even?
+      num /= 2
+      n += 1
+    else
+      num -= 1
+      n += 1
+    end
+  end
+  puts n
+end
+
+number_of_steps(14)
